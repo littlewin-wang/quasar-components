01ad6efd2bb7abf8c58d18b51c08a292b2e37f31
littlewin <littlewin.wang@sky-cloud.net>
2017-07-14 16:04:51 +0800

    update breadcrumb componnet

---
+++ src/components/BreadCrumb/BreadCrumb.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="bread-crumb text-primary">
+    <span v-for="(list, index) in breadList" v-bind:key="list.title">
+      <button class="bread-item clear">
+        {{list.title}}
+      </button>
+      <span v-if="index < breadList.length - 1"> / </span>
+    </span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      breadList: Array
+    }
+  }
+</script>
+
+<style lang="stylus" scoped>
+  .bread-item
+    padding: 0
+</style>
